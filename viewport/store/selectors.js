9e7d7f9ef31ba8fb7944f8430db02762b34d8996
Andrew Duthie <andrew@andrewduthie.com>
2018-02-22 16:37:00 -0500

    Framework: Add viewport module

---
+++ viewport/store/selectors.js
@@ -0,0 +1,21 @@
+/**
+ * Returns true if the viewport matches the given query, or false otherwise.
+ *
+ * @param {Object} state Viewport state object.
+ * @param {string} query Query string. Includes operator and breakpoint name,
+ *                       space separated. Operator defaults to >=.
+ *
+ * @example
+ *
+ * ```js
+ * isViewportMatch( state, '< huge' );
+ * isViewPortMatch( state, 'medium' );
+ * ```
+ *
+ * @return {boolean} Whether viewport matches query.
+ */
+export function isViewportMatch( state, query ) {
+	const key = [ '>=', ...query.split( ' ' ) ].slice( -2 ).join( ' ' );
+
+	return !! state[ key ];
+}
